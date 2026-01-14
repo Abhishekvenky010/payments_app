66f21a6f36dce41cdac8003f92f5ab2cb7897941
Abhishekvenky010 <abhishekvenky010@gmail.com>
2026-01-14 23:20:27 +0530

    done on ramp transfer endpoints

--- backend/src/routes/merchant.ts
+++ backend/src/routes/merchant.ts
@@ -1,46 +1,63 @@
-import {Router} from "express";
-import {PrismaClient} from "@prisma/client";
-import jwt from "jsonwebtoken";
-import { JWT_PASS } from "../config";
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+    import jwt from "jsonwebtoken";
+import { JWT_MERCHANT_PASS } from "../config";
+
 const prismaClient = new PrismaClient();
+
 export const merchantRouter = Router();
-merchantRouter.post("/signup",async (req,res)=>{
-  const {username,password,name} = req.body;
-  try{
-    await prismaClient.merchant.create({
-        data:{
-            username,
-            password,
-            name
-        }
-    })
-    res.json({
-        message : "signedup succesfully"
-    })
-  }catch(e){
-    return res.status(403).json({
-        message : "error while signing up"
-    })
-  }
-})
-merchantRouter.post("/signin",async (req,res)=>{
-    const  {username,password} = req.body;
-    const merchant = await prismaClient.merchant.findFirst({
-       where:{
-        username,
-        password
-       }
-    })
-    if(!merchant){
+
+merchantRouter.post("/signup", async (req, res) => {
+    const { username, password, name } = req.body; // zod to verify the schema
+
+    try {
+        await prismaClient.$transaction(async tx => {
+            const merchant = await tx.merchant.create({
+                data: {
+                    username,
+                    password,
+                    name
+                }
+            })
+
+            await tx.merchantAccount.create({
+                data: {
+                    merchantId: merchant.id
+                }
+            })
+        })
+
+        res.json({
+            message: "Signed up"
+        })
+    } catch(e) {
         return res.status(403).json({
-            message : "unable to login"
+            message: "Error while signing up"
         })
     }
+    
+})
+
+merchantRouter.post("/signin", async (req, res) => {
+    const { username, password } = req.body;
+
+    const merchant = await prismaClient.merchant.findFirst( {
+        where: {
+            username,
+            password
+        }
+    })
+    if (!merchant) {
+        return res.status(403).json({
+            message: "Unable to log you in"
+        })
+    }
+
     const token = jwt.sign({
-        id: merchant.id
-    },JWT_PASS)
+        id: merchant.id,
+    }, JWT_MERCHANT_PASS)
 
     return res.json({
         token
-    })
-})
+    }) 
+}) 
